2f5604d0c734cededddaf0d88e51e81a0a0e879e
Elvis  Asante <elvisasante5@yahoo.com>
2022-06-10 06:40:54 +0100

    Installed node-modules
    Installed node-modules

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 // Import http package from node_modules
 const http = require('http');
 
-const server = http.create
+// Create a server response
+const server = http.createServer((req, res) => {
+    res.end('This is my server response!');
+});
+
+// Listen for requests on environment port or 3000 for development
+server.listen(process.env.PORT || 3000);
