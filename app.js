db552778724cf7992d2c8876af39de3bfacb0cfb
Elvis  Asante <elvisasante5@yahoo.com>
2022-06-16 06:15:56 +0100

    CRUD operations integrated
    API now has CRUD operations integrated

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-// Import express library
-const express = require('express');
+const express = require('express'); // Import express library to create a server
+const bodyParser = require('body-parser'); // Import body-parser to extract request body
+const mongoose = require('mongoose'); // Import mongoose to connect to MongoDB cluster
+const Thing = require('./models/thing'); // Import model from data schema to perform DB operations
 
 // Run express
 const app = express();
+
+// Connect to MongoDB cluster
+mongoose.connect('mongodb+srv://elvisasante5:psnahBmwOj0I7xEp@cluster0.yx17j3o.mongodb.net/?retryWrites=true&w=majority')
+    .then( () => {
+        console.log('Successfully connected to MongoDB cluster !');
+    })
+    .catch( (error) => {
+        console.log('Unable to connect to cluster !');
+        console.log(error);
+    });
 
 // Set headers to prevent Cors errors
 app.use((req, res, next) => {
@@ -13,38 +25,66 @@ app.use((req, res, next) => {
 });
 
 // Extract JSON body from request
-app.use(express.json());
+app.use(bodyParser.json());
 
-// Endpoint '/api/stuff' and it's API method is POST
+// Saving an item
 app.post('/api/stuff',(req, res, next) => {
-    console.log(req.body)
-    res.status(201).json({ message: 'Data persisted successfully'});
+
+    const thing = new Thing({
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId
+    });
+
+    thing.save()
+        .then( () => { res.status(201).json({ message: 'Post saved successfully!'}); })
+        .catch( (error) => { res.status(400).json({ error: error }); })
 });
 
-// Endpoint '/api/stuff' and it's API method is GET
+// Getting all items
 app.get('/api/stuff',(req, res, next) => {
-    const stuff = [
-        {
-            _id: 'bn1',
-            title: 'My first thing',
-            description: 'All of the info about my first thing',
-            imageUrl: 'https://sm.pcmag.com/t/pcmag_uk/review/m/msi-katana/msi-katana-gf66_h1g7.1920.jpg',
-            price: 4900,
-            userId: '1'
-        },
-        {
-            _id: 'bn2',
-            title: 'My second thing',
-            description: 'All of the info about my second thing',
-            imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZQWxyyNX3uPXO3_2KqWya0LR6i8CyUfSDgHIGmR0pgZYUI0D4x8u0ic6xrkRkvNaisS0&usqp=CAU',
-            price: 2900,
-            userId: '2'
-        }
-    ];
 
-    res.status(200).json(stuff);
+    Thing.find()
+        .then( (things) => { res.status(200).json({ message: things}) } )
+        .catch( (error) => { res.status(400).json({ error: error }) } )
 });
 
+// Getting a specific item
+app.get('/api/stuff/:id', (req, res, next) => {
+
+    Thing.findOne({ _id: req.params.id} )
+        .then((thing) => { res.status(200).json(thing); })
+        .catch( (error) => { res.status(404).json({ error: error } ); }
+    );
+});
+
+// Updating an item
+app.put('/api/stuff/:id',(req, res, next) => {
+
+    const thing = new Thing({
+        _id: req.params.id,
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId
+    });
+
+    thing.updateOne( { _id: req.params.id }, thing )
+        .then( () => { res.status(201).json({ message: 'Item updated successfully!'}); })
+        .catch( (error) => { res.status(400).json({ error: error }); })
+});
+
+// Deleting an item
+app.delete('/api/stuff/:id', (req, res, next) => {
+
+    Thing.deleteOne({ _id: req.params.id} )
+        .then((thing) => { res.status(200).json({ message: 'Item has been deleted'}); })
+        .catch( (error) => { res.status(404).json({ error: error } ); }
+        );
+});
 
 // Export const app to make it accessible somewhere else
 module.exports = app;
